6c377d0b2ed0fda31d4378189a5a02138ab2e2c6
nvcleemp <nico.vancleemput@gmail.com>
2018-03-15 15:36:18 +0100

    Fixed imports

--- qdge/gui/GraphPanel.java
+++ qdge/gui/GraphPanel.java
@@ -22,8 +22,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.Stroke;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
